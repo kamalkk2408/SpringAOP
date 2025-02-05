f86d20ef0cd489db04ab7dc35fd53ebf0925e587
kamalkk2408 <kamalkishore2408@gmail.com>
2025-02-06 01:24:59 +0530

    imports corrected & basic java app maven setup done

--- src/main/java/com/kishore/kamal/spring_aop/SpringAopApplication.java
+++ src/main/java/com/kishore/kamal/spring_aop/SpringAopApplication.java
@@ -1,13 +1,12 @@
 package com.kishore.kamal.spring_aop;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringAopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAopApplication.class, args);
+
+		System.out.println("Hello World");
+//		SpringApplication.run(SpringAopApplication.class, args);
 	}
 
 }
