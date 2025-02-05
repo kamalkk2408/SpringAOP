957e88e88f009f10493779e7c9698dcc65872bd9
kamalkk2408 <kamalkishore2408@gmail.com>
2025-02-06 01:27:53 +0530

    imports aspectj dependencies

--- src/main/java/com/kishore/kamal/spring_aop/SpringAopApplication.java
+++ src/main/java/com/kishore/kamal/spring_aop/SpringAopApplication.java
@@ -1,12 +1,9 @@
 package com.kishore.kamal.spring_aop;
 
-//@SpringBootApplication
 public class SpringAopApplication {
 
 	public static void main(String[] args) {
-
 		System.out.println("Hello World");
-//		SpringApplication.run(SpringAopApplication.class, args);
 	}
 
 }
