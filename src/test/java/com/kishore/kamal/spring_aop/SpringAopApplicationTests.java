62aa97b8444784869180ba99bcb5cbc441848d69
kamalkk2408 <kamalkishore2408@gmail.com>
2025-02-05 23:40:25 +0530

    spring initialized with pom files

---
+++ src/test/java/com/kishore/kamal/spring_aop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishore.kamal.spring_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
